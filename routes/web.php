2981a969650901e605cf522c07fb531395b7958c
Edimarf <edimarf09@gmail.com>
2022-12-01 12:12:28 -0300

    atualizacao
    estudo ate o dia 1 de dezembro

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\FornecedorController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,7 @@ Route::get('/',[MainController::class, 'index'])->name('site.index');
 Route::get('/sobre-nos',[SobreNosController::class, 'index'])->name('site.sobrenos');
 
 Route::get('/contato',[ContatoController::class, 'index'])->name('site.contato');
+Route::post('/contato',[ContatoController::class, 'index'])->name('site.contato');
 
 Route::get('/login')->name('site.login');
 
@@ -28,13 +31,14 @@ Route::prefix('/app')->group( function(){
     
     Route::get('/clientes')->name('app.clientes');
     
-    Route::get('/fornecedores')->name('app.fornecedores');
+    Route::get('/fornecedores',[FornecedorController::class, 'index'])->name('app.fornecedores');
     
     Route::get('/produtos')->name('app.produtos');
 
 });
 
 Route::fallback(function() {
-    return redirect(route('site.index'));
+    echo 'A rota acessada não existe <a href="'.route('site.index').'">clique aqui</a> para voltar a navegação normal';
 });
 
+Route::get('/teste/{p1}/{p2}',[TesteController::class, 'index'])->name('teste');
